063d071c8d956df6e9e2f1fd0afe9931f1d8e6c9
Jesus Arturo Hdz Soberon <jasoberon@gmail.com>
2018-07-18 14:21:17 -0600

    Add files via upload
    Code for Arduino

---
+++ sensor_presion.ino
@@ -0,0 +1,23 @@
+//código Arduino para sensor SKU237545
+
+void setup()
+{
+  Serial.begin(9600);
+ }
+ 
+void loop()
+{
+ int sensorVal=analogRead(A1);   //variable guarda lectura analógica
+ Serial.print("Sensor Value: ");
+ Serial.print(sensorVal);
+ float voltage = (sensorVal*5.0)/1024.0;
+ Serial.print("Volts: ");
+ Serial.print(voltage);
+ float pressure_pascal = (3.0*((float)voltage-0.47))*1000000.0;  //cambio a pascales
+ float pressure_bar = pressure_pascal/10e5;                      //cambio a bares
+ Serial.print("Pressure = ");
+ Serial.print(pressure_bar);
+ Serial.println(" bars");
+ Serial.print("Pressure = ");
+ delay(100);
+ }
